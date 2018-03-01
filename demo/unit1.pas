68e32995d4be087750ff954e3cc14ed86d11bd68
Alexey-t <support@uvviewsoft.com>
2018-03-01 10:13:22 +0300

    update demo

--- demo/unit1.pas
+++ demo/unit1.pas
@@ -6,21 +6,28 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
-  atimagebox;
+  ExtDlgs, atimagebox;
 
 type
   { TForm1 }
 
   TForm1 = class(TForm)
+    btnOpen: TButton;
+    chkFitBig: TCheckBox;
     chkFitToWindow: TCheckBox;
     chkCenter: TCheckBox;
+    LabelStatus: TLabel;
+    OpenPictureDialog1: TOpenPictureDialog;
     Panel1: TPanel;
     Panel2: TPanel;
+    procedure btnOpenClick(Sender: TObject);
     procedure chkCenterChange(Sender: TObject);
+    procedure chkFitBigChange(Sender: TObject);
     procedure chkFitToWindowChange(Sender: TObject);
     procedure FormCreate(Sender: TObject);
   private
     box: TATImagebox;
+    procedure UpdateStatus(Sender: TObject);
   public
 
   end;
@@ -41,6 +48,12 @@ begin
   box.Align:= alClient;
   box.ImageCenter:= true;
   box.LoadFromFile(ExtractFilePath(Application.ExeName)+'test.jpg');
+  box.OnScroll:= @UpdateStatus;
+end;
+
+procedure TForm1.UpdateStatus(Sender: TObject);
+begin
+  LabelStatus.Caption:= IntToStr(box.ImageScale)+'%';
 end;
 
 procedure TForm1.chkFitToWindowChange(Sender: TObject);
@@ -51,6 +64,19 @@ end;
 procedure TForm1.chkCenterChange(Sender: TObject);
 begin
   box.ImageCenter:= chkCenter.Checked;
+end;
+
+procedure TForm1.btnOpenClick(Sender: TObject);
+begin
+  if OpenPictureDialog1.Execute then
+  begin
+    box.LoadFromFile(OpenPictureDialog1.FileName);
+  end;
+end;
+
+procedure TForm1.chkFitBigChange(Sender: TObject);
+begin
+  box.ImageFitOnlyBig:= chkFitBig.Checked;
 end;
 
 end.
